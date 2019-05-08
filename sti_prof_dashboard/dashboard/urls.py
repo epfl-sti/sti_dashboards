469afd44fbb0bdb773056deca29b1e6bebc04508
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-05-08 15:27:34 +0200

    Added first Tableau view

--- sti_prof_dashboard/dashboard/urls.py
+++ sti_prof_dashboard/dashboard/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('credits', views.ECTS_credits_by_teacher, name="ECTS_credits_by_teacher" ),
     path('icons/icons.svg', RedirectView.as_view(url="/static/epfl/elements/icons/icons.svg")),
 ]
--- sti_prof_dashboard/dashboard/views.py
+++ sti_prof_dashboard/dashboard/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def ECTS_credits_by_teacher(request):
+    context = {'chart_name': u'STIfacultiesdashboard&#47;TotalnumberofECTScreditstaughtbyteachersplitbyteacherrank'}
+    return render(request, 'tableau.html', context=context)
