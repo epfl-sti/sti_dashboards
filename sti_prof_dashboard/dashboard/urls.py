300aa4dca22f39cecddc7a3a19768a5b50365a18
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-05-08 14:14:07 +0200

    implemented the 2018 graphical layout of EPFL

--- sti_prof_dashboard/dashboard/urls.py
+++ sti_prof_dashboard/dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('icons/icons.svg', RedirectView.as_view(url="/static/epfl/elements/icons/icons.svg")),
 ]
