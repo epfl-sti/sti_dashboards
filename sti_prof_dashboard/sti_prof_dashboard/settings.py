81868371c1269dc0c7483ca3700221fe7920dbd1
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-05-08 09:11:46 +0200

    fixed debug switching based on envrionment

--- sti_prof_dashboard/sti_prof_dashboard/settings.py
+++ sti_prof_dashboard/sti_prof_dashboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'ChangeMe!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG') or False
 
 ALLOWED_HOSTS = []
 
