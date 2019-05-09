6159d1d9670653565b01242bfdbdd174e2e24c99
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-05-09 16:31:45 +0200

    componentized placeholder pages. added section pages for teaching, finance and campus

--- sti_prof_dashboard/dashboard/urls.py
+++ sti_prof_dashboard/dashboard/urls.py
@@ -6,9 +6,15 @@ app_name = "dashboard"
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('teaching/ECTS_rankings', views.ECTS_credits_rankings, name="ECTS_credits_rankings" ),
-    path('teaching/ECTS_credits_details', views.ECTS_credits_details, name="ECTS_credits_details" ),
-    path('teaching/teaching_hours_rankings', views.teaching_hours_rankings, name="teaching_hours_rankings"),
+    path('teaching', views.teaching_sub_section, name="teaching"),
+    path('teaching/ECTS_rankings', views.ECTS_credits_rankings,
+         name="ECTS_credits_rankings"),
+    path('teaching/ECTS_credits_details',
+         views.ECTS_credits_details, name="ECTS_credits_details"),
+    path('teaching/teaching_hours_rankings',
+         views.teaching_hours_rankings, name="teaching_hours_rankings"),
+    path('finance', views.finance_sub_section, name="finance"),
     path('finance/finances', views.finances, name="finances"),
+    path('campus', views.campus_sub_section, name="campus"),
     path('campus/space_used', views.space_used, name="space_used"),
 ]
--- sti_prof_dashboard/dashboard/views.py
+++ sti_prof_dashboard/dashboard/views.py
@@ -7,7 +7,11 @@ from django.views.decorators.cache import cache_control
 @cache_page(60 * 15)
 @cache_control(max_age=3600)
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'image_relative_path': 'dashboard/img/image_largeprvw.jpeg',
+        'title': 'STI faculties dashboards',
+        'details': "You will find a bird's eye view on the statistics related to your activities at STI. Dashboards on data such as the number of ECTS credits you taught can be found in the menu on the left hand side."}
+    return render(request, 'generic_section_page.html', context=context)
 
 
 @cache_page(60 * 15)
@@ -44,3 +48,33 @@ def finances(request):
 def space_used(request):
     context = {'chart_name': 'STIfacultiesdashboard-Archibus&#47;space'}
     return render(request, 'tableau.html', context=context)
+
+
+@cache_page(60 * 15)
+@cache_control(max_age=3600)
+def campus_sub_section(request):
+    context = {
+        'image_relative_path': 'dashboard/img/sven-mieke-1162927-unsplash.jpg',
+        'title': 'Campus',
+        'details': 'You will find information related to your physical occupation of the campus.'}
+    return render(request, 'generic_section_page.html', context=context)
+
+
+@cache_page(60 * 15)
+@cache_control(max_age=3600)
+def teaching_sub_section(request):
+    context = {
+        'image_relative_path': 'dashboard/img/ross-sneddon-798471-unsplash.jpg',
+        'title': 'Teaching',
+        'details': 'You will find information related to your teaching activities at STI'}
+    return render(request, 'generic_section_page.html', context=context)
+
+
+@cache_page(60 * 15)
+@cache_control(max_age=3600)
+def finance_sub_section(request):
+    context = {
+        'image_relative_path': 'dashboard/img/arseny-togulev-1166920-unsplash.jpg',
+        'title': 'Finance',
+        'details': 'You will find information related to the financing of your activities at STI'}
+    return render(request, 'generic_section_page.html', context=context)
