a872aef590ef87a87e5feb5c0c21bbb9f56b961d
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-05-09 11:12:46 +0200

    added app_name to avoid names collisions

--- sti_prof_dashboard/dashboard/urls.py
+++ sti_prof_dashboard/dashboard/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.views.generic import RedirectView
 from . import views
 
+app_name = "dashboard"
+
 urlpatterns = [
     path('', views.index, name="index"),
     path('credits', views.ECTS_credits_by_teacher, name="ECTS_credits_by_teacher" ),
