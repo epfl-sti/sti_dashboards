2d4e0ec58153deab1dce84f7150c076dacaa8a51
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-05-09 16:03:53 +0200

    improved caching

--- sti_prof_dashboard/dashboard/views.py
+++ sti_prof_dashboard/dashboard/views.py
@@ -10,27 +10,37 @@ def index(request):
     return render(request, 'index.html')
 
 
+@cache_page(60 * 15)
+@cache_control(max_age=3600)
 def ECTS_credits_rankings(request):
     context = {'chart_name': 'STIfacultiesdashboard&#47;ECTScreditsrankings'}
     return render(request, 'tableau.html', context=context)
 
 
+@cache_page(60 * 15)
+@cache_control(max_age=3600)
 def ECTS_credits_details(request):
     context = {
         'chart_name': 'STIfacultiesdashboard&#47;DetailednumberofECTScreditstaughtbyteacher'}
     return render(request, 'tableau.html', context=context)
 
 
+@cache_page(60 * 15)
+@cache_control(max_age=3600)
 def teaching_hours_rankings(request):
     context = {'chart_name': 'STIfacultiesdashboard&#47;Teachinghoursrankings'}
     return render(request, 'tableau.html', context=context)
 
 
+@cache_page(60 * 15)
+@cache_control(max_age=3600)
 def finances(request):
     context = {'chart_name': 'STIfacultiesdashboard-SAPFI&#47;finances'}
     return render(request, 'tableau.html', context=context)
 
 
+@cache_page(60 * 15)
+@cache_control(max_age=3600)
 def space_used(request):
     context = {'chart_name': 'STIfacultiesdashboard-Archibus&#47;space'}
     return render(request, 'tableau.html', context=context)
