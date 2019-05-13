00a9fb7ee4323631da0048ea198dc11ae141dc03
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-05-13 15:06:51 +0200

    moved env sepcific data to environment variables

--- sti_prof_dashboard/sti_prof_dashboard/settings.py
+++ sti_prof_dashboard/sti_prof_dashboard/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'ChangeMe!'
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG') or False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
