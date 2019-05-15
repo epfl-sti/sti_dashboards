8925ca9828f83ea32442ae10b12f623c787eae8c
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-05-15 08:34:48 +0200

    dynamically load dotenv from wsgi

--- sti_prof_dashboard/sti_prof_dashboard/wsgi.py
+++ sti_prof_dashboard/sti_prof_dashboard/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv, find_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
+load_dotenv(find_dotenv())
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sti_prof_dashboard.settings")
 
 application = get_wsgi_application()
