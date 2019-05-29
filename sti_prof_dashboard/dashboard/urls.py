523ebbc4f5c0cc8129592a78e444695d71d2788d
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-05-29 16:26:43 +0200

    cleanup des urls de test.

--- sti_prof_dashboard/dashboard/urls.py
+++ sti_prof_dashboard/dashboard/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     path('finance/budgets', views.budgets, name="budgets"),
     path('campus', views.campus_sub_section, name="campus"),
     path('campus/space_used', views.space_used, name="space_used"),
-    path('test_viz', views.test_tableau_viz, name="test_viz"),
-    path('test_ldap', views.test_ldap, name="test_ldap"),
+#     path('test_viz', views.test_tableau_viz, name="test_viz"),
+#     path('test_ldap', views.test_ldap, name="test_ldap"),
 ]
