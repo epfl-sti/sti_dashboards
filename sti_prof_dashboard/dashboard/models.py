fa76ad887db492690d1f694f646342c487455599
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-09-09 11:11:59 +0200

    Updated to have real avatar

--- sti_prof_dashboard/dashboard/models.py
+++ sti_prof_dashboard/dashboard/models.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from url_or_relative_url_field.fields import URLOrRelativeURLField
 
 
 class Person(AbstractUser):
     sciper = models.IntegerField(null=True, blank=True, default=None)
+    is_dean = models.BooleanField(null=False, blank=False, default=False)
+    is_institute_manager = models.BooleanField(null=False, blank=False, default=False)
+    picture_url = URLOrRelativeURLField(blank=True, null=True, default=None)
 
 # objects not existing in the database
 
