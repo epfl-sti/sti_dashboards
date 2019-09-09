3c7e73a338056a3de420657de54184f9c04a290d
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-09-09 10:19:31 +0200

    Moved to custom authentication model

--- sti_prof_dashboard/dashboard/models.py
+++ sti_prof_dashboard/dashboard/models.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-####### objects not existing in the database
+
+class Person(AbstractUser):
+    sciper = models.IntegerField(null=True, blank=True, default=None)
+
+# objects not existing in the database
+
+
 class NextStep(object):
     def __init__(self, title=None, description=None, link=None):
         self.title = title
