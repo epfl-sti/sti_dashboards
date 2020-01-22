e2435725c7b73ec6abeb5567b8cca11945f83c3b
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2020-01-22 14:34:11 +0100

    displays the photo of the person using EPFL people endpoint

--- sti_prof_dashboard/dashboard/context_processors.py
+++ sti_prof_dashboard/dashboard/context_processors.py
@@ -1,4 +1,6 @@
-from ldap3 import ALL, Connection, Server, LEVEL
+import requests
+from django.conf import settings
+from ldap3 import ALL, LEVEL, Connection, Server
 
 
 def get_institutes(request):
@@ -12,3 +14,20 @@ def get_institutes(request):
     for entry in conn.entries:
         institutes[min(entry['ou'], key=len)] = entry['description;lang-en']
     return {'INSTITUTES': institutes}
+
+
+def get_photo_url(request):
+    """
+    Queries the EPFL People web service to return the url of the currently logged in user's picture
+    """
+    return_value = '/static/dashboard/img/user.png'
+    url = settings.PEOPLE_WS_ENDPOINT.format(request.user.sciper)
+    r = requests.get(url)
+    if r.status_code == 200:
+        data = r.json()
+        photo_should_be_displayed = bool(int(data[str(request.user.sciper)]['people']['photo_show']))
+        if photo_should_be_displayed:
+            photo_url = data[str(request.user.sciper)]['people']['photo_url']
+            return_value = photo_url
+
+    return {'PHOTO_URL': return_value}
--- sti_prof_dashboard/settings/base.py
+++ sti_prof_dashboard/settings/base.py
@@ -148,3 +148,5 @@ APP_BASE_URL = os.environ.get('DJANGO_APP_BASE_URL', 'https://localhost')
 
 
 STI_INSTITUTES = ['IBI-STI', 'IEL', 'IGM', 'IMT', 'IMX']
+
+PEOPLE_WS_ENDPOINT = 'https://people.epfl.ch/cgi-bin/wsgetpeople?scipers={}'
